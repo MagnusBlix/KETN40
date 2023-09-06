1624d74c076f11e95e86492fb5a6ab79f582dccc
MagnusBlix <138386477+MagnusBlix@users.noreply.github.com>
2023-09-06 14:52:29 +0200

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Variables
+
+Created on Wed Sep  6 13:39:06 2023
+
+@author: Magnus
+"""
+
+v = {}
+
+v['H_0A'] = 1.2e-5
+v['H_0B'] = 3.6e-6
+v['H_0D'] = 3.10e-4
+v['beta_ABC'] = 8.41
+v['beta_D'] = 4.57
+v['q_max'] = 400    # g/L
+v['k_A'] = 15   #k_kin [h-1]
+v['k_BC'] = 18
+v['k_D'] = 43.4 
+
+v['eps_c'] = 0.42 # column void
+v['eps_p'] = 0.62 # packing porosity
+v['pe'] = 0.5
+# v['extinct'] = 1 # (L/g/cm)
+# v['UV cell path'] = 1 cm
